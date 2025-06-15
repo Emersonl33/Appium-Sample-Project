2b0aad4ad715c188f449a1348c38873336cb0a66
unknown <emersonl33@hotmail.com>
2025-06-15 12:26:28 -0300

    refactor android findby annotation

--- src/test/java/appiumcucumber/kabum/pages/CartPage.java
+++ src/test/java/appiumcucumber/kabum/pages/CartPage.java
@@ -1,11 +1,12 @@
 package appiumcucumber.kabum.pages;
 
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class CartPage{
 
-    @FindBy (id = "br.com.kabum.webviewapp:id/description")
+    @AndroidFindBy(id = "br.com.kabum.webviewapp:id/description")
     public WebElement descriptionProductLabel;
 
 
--- src/test/java/appiumcucumber/kabum/pages/HomePage.java
+++ src/test/java/appiumcucumber/kabum/pages/HomePage.java
@@ -1,24 +1,23 @@
 package appiumcucumber.kabum.pages;
 
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-
 
 public class HomePage {
 
-        @FindBy(id = "br.com.kabum.webviewapp:id/btn_close")
+        @AndroidFindBy(id = "br.com.kabum.webviewapp:id/btn_close")
         public WebElement buildPcCloseButton;
 
-        @FindBy(id = "br.com.kabum.webviewapp:id/search_edit_frame")
+        @AndroidFindBy(id = "br.com.kabum.webviewapp:id/search_edit_frame")
         public WebElement searchBarInput;
 
-        @FindBy(xpath = "//android.widget.TextView[@resource-id=\"br.com.kabum.webviewapp:id/name\" and @text=\"Placa de Vídeo Gigabyte RTX 5070 AERO 12G NVIDIA GeForce, 12GB GDDR7, 192bits, RGB, DLSS, Ray Tracing - 9VN5070AO-00-G10\"]")
+        @AndroidFindBy(xpath = "//android.widget.TextView[@resource-id=\"br.com.kabum.webviewapp:id/name\" and @text=\"Placa de Vídeo Gigabyte RTX 5070 AERO 12G NVIDIA GeForce, 12GB GDDR7, 192bits, RGB, DLSS, Ray Tracing - 9VN5070AO-00-G10\"]")
         public WebElement gpuProductLabel;
 
-        @FindBy(xpath = "(//android.widget.ImageButton[@content-desc=\"Comprar\"])[4]")
+        @AndroidFindBy(xpath = "(//android.widget.ImageButton[@content-desc=\"Comprar\"])[4]")
         public WebElement addGpuProductButton;
 
-        @FindBy(id = "br.com.kabum.webviewapp:id/op_basket")
+        @AndroidFindBy(id = "br.com.kabum.webviewapp:id/op_basket")
         public WebElement cartMenuButton;
 
 
